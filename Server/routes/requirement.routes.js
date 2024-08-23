0d48f3527bb3dc7a784abe088cc26644aa4fde28
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2024-08-23 19:24:43 +0000

    next changes

--- Server/routes/requirement.routes.js
+++ Server/routes/requirement.routes.js
@@ -1,15 +1,21 @@
 import express from "express";
 
 import {
-  createUser,
-  getAllUsers,
-  getUserInfoByID,
-} from "../controllers/user.controller.js";
+    saveRequirement,
+    getAllRequirements,
+    getRequirementById,
+    deleteRequirement,
+    updateRequirement,
+    getTopLatestRequirements,
+} from "../controllers/requirement.controller.js";
 
 const router = express.Router();
 
-router.route("/").get(getAllUsers);
-router.route("/").post(createUser);
-router.route("/:id").get(getUserInfoByID);
+router.route("/").post(saveRequirement);
+router.route("/").get(getAllRequirements);
+router.route("/five").get(getTopLatestRequirements);
+router.route("/:id").get(getRequirementById);
+router.route("/:id").delete(deleteRequirement);
+router.route("/:id").patch(updateRequirement);
 
 export default router;
